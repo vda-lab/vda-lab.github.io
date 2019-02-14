31cb9a1ee3afc89b246a93802200bfcfabded968
Jan Aerts <jan.aerts@gmail.com>
2019-02-14 09:35:58 +0100

    Added script for i0u19a

---
+++ tidbits/relations.sql
@@ -0,0 +1,13 @@
+CREATE TABLE relations (
+  knower STRING,
+  knowee STRING
+);
+
+INSERT INTO relations (knower, knowee) VALUES ('Tim','Terry');
+INSERT INTO relations (knower, knowee) VALUES ('Tom','Terry');
+INSERT INTO relations (knower, knowee) VALUES ('Terry','Gerry');
+INSERT INTO relations (knower, knowee) VALUES ('Gerry','Rik');
+INSERT INTO relations (knower, knowee) VALUES ('Gerry','James');
+INSERT INTO relations (knower, knowee) VALUES ('James','John');
+INSERT INTO relations (knower, knowee) VALUES ('Fred','James');
+INSERT INTO relations (knower, knowee) VALUES ('Frits','Fred');
